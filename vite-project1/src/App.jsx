689b58952652a8568bdda33babd8dbe794209e03
Your Name <you@example.com>
2026-06-02 03:18:22 +0530

    Learned to create folder and understood the folder structure through vite

---
+++ vite-project1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <div id='parent'>
+    //   <h1 id='child1'>I am starting  react for my blockchain </h1>
+    //   <h2 id='child2'> we can not return two things</h2>
+    // </div>
+    // if we want to return two things then we have to wrap them in a div or we can use react fragment
+
+    <>
+    <div1>here we have </div1>
+    <div2>new idea </div2>
+    </>
+
+  )
+}
+
+export default App
